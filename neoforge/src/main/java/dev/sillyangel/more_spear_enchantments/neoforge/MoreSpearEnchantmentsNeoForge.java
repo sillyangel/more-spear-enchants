23dfcdc3698f96c74e3902ff6745bc1f1ccbc232
angel <angel@sillyangel.dev>
2026-01-01 23:31:01 -0600

    1.0.1

---
+++ neoforge/src/main/java/dev/sillyangel/more_spear_enchantments/neoforge/MoreSpearEnchantmentsNeoForge.java
@@ -0,0 +1,14 @@
+package dev.sillyangel.more_spear_enchantments.neoforge;
+
+import net.neoforged.fml.common.Mod;
+
+import dev.sillyangel.more_spear_enchantments.MoreSpearEnchantments;
+
+@Mod(MoreSpearEnchantments.MOD_ID)
+public final class MoreSpearEnchantmentsNeoForge {
+    public MoreSpearEnchantmentsNeoForge() {
+        // Run our common setup.
+        MoreSpearEnchantments.init();
+    }
+}
+
